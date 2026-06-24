aa0c23fc55b9a910bb7acf1d901548b74a152baf
Demo for Git <rejankkarkiv2@gmail.com>
2026-06-24 13:23:20 -0500

    feat: add validation and error middleware

--- schema.js
+++ schema.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 
+// 1. Existing Listing Schema Validation
 module.exports.listingSchema = Joi.object({
     listing: Joi.object({
         title: Joi.string().required(),
@@ -7,6 +8,17 @@ module.exports.listingSchema = Joi.object({
         location: Joi.string().required(),
         country: Joi.string().required(),
         price: Joi.number().required().min(0),
-        image: Joi.string().allow("", null)
+        image: Joi.object({
+            url: Joi.string().allow("", null),
+            filename: Joi.string().allow("", null),
+        }).default({})
     }).required()
+});
+
+// 2. NEW: Review Schema Validation
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required(),
+    }).required(),
 });
