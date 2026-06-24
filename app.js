7bacd595ca56607327be8e615b74e5913d279b5e
Demo for Git <rejankkarkiv2@gmail.com>
2026-06-24 13:23:39 -0500

    chore: update app configuration

--- app.js
+++ app.js
@@ -1,115 +1,91 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
-const mongoose = require ("mongoose");
-const Listing = require ("./models/listing.js" );
-const path = require ("path") ;
-const methodOverride = require ("method-override");
-const ejsMate = require ("ejs-mate");
-const wrapAsync = require ("./utils/wrapAsync.js");
-const ExpressError = require ("./utils/ExpressError.js");
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const session = require ("express-session");
+const flash = require ("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-
 main().then(() => {
-    console.log ("connected to DB")
+    console.log("connected to DB");
 }) 
 .catch((err) => {
-    console.log (err);
-})
-
-async function main(){
-    await mongoose.connect (MONGO_URL)
-}
-
-app.set ("view engine" , "ejs");
-app.set ("views" , path.join (__dirname , "views"));
-app.use (express.urlencoded({extended : true}));
-app.use (methodOverride("_method"));
-app.engine('ejs', ejsMate);
-app.use (express.static(path.join(__dirname, "/public")));
-const { listingSchema } = require("./schema.js");
-
-app.get ("/" , (req, res ) => {
-    res.send ("Hi, I am the root ")
-})
-
-
-const validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-};
-
-
-//  index route 
-app.get ("/listings" , wrapAsync(async(req , res) => {
-   const allListings = await Listing.find({});
-res.render("listings/index.ejs", { allListings });
-}))
-
-// New route 
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs"); 
+    console.log(err);
 });
 
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 
- // Create Route --> works on new route and add to mongo 
-//Create Route
-app.post(
-    "/listings",
-    validateListing,
-    wrapAsync(async (req, res, next) => {
-        
-        const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-    })
-);
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true
+    },
+};
 
+app.get("/", (req, res) => {
+    res.send("Hi, I am the root ");
+});
 
-// Edit route 
-app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-}));
+app.use(session(sessionOptions));
+app.use (flash());
 
-// update route 
-app.put(
-    "/listings/:id", 
-    validateListing, 
-    wrapAsync(async (req, res) => {
-        let { id } = req.params;
-        await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-        res.redirect(`/listings/${id}`); 
-    })
-);
+//  CORRECT
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate())); // <-- Make sure this says authenticate()
 
-// delete route 
-app.delete ("/listings/:id", wrapAsync(async (req , res ) => {
-    let {id } = req.params ;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log (deletedListing);
-    res.redirect ("/listings");
-}))
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-// show route 
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", { listing });
-}));
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
+});
 
-// error 
+// demo pass
+// app.get("/demouser", async (req, res) => {
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username: "delta-student"
+//     });
+
+//     let registeredUser = await User.register(fakeUser, "helloworld");
+//     res.send(registeredUser);
+// });
+
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/", userRouter);
+
+// Error handling
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
 });
@@ -119,8 +95,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error.ejs", { message });
 });
 
-
-app.listen (8080, () => {
-    console.log ("the server is listening to the port ");
-
-})
+app.listen(8080, () => {
+    console.log("the server is listening to the port 8080");
+});
