17b810a6af9fb9d9dcbbfb5011dc10b58d94a832
Demo for Git <rejankkarkiv2@gmail.com>
2026-06-24 13:23:14 -0500

    feat: update review routes

---
+++ routes/review.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const Listing = require("../models/listing.js");
+const Review = require("../models/review.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const {
+    isLoggedIn,
+    isReviewAuthor,
+    validateReview,
+} = require("../middleware.js");
+
+// Post Review Route
+router.post(
+    "/",
+    isLoggedIn,
+    validateReview,
+    wrapAsync(async (req, res) => {
+        let listing = await Listing.findById(req.params.id);
+        let newReview = new Review(req.body.review);
+        newReview.author = req.user._id;
+        listing.reviews.push(newReview);
+
+        await newReview.save();
+        await listing.save();
+        req.flash("success", "New Review Created!");
+        res.redirect(`/listings/${listing._id}`);
+    })
+);
+
+// Delete Review Route
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash("success" , "review deleted ");
+    res.redirect(`/listings/${id}`);
+}));
+
+module.exports = router;
